82147986f857e05e53d899dd859db8f628c73314
steves3142 <steves3142@gmail.com>
2023-01-10 12:04:15 -0500

    moving files

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import type { NextPage} from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+    <Head>
+      <title>MSNT Telecom</title>
+    </Head>
+    This is a test
+    </div>
+  )
+};
+
+export default Home;
