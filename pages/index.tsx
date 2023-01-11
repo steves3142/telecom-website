d502142e0c4dfb0a89af2cdda08d25a567ee89fd
steves3142 <steves3142@gmail.com>
2023-01-11 15:21:55 -0500

    hero and footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,26 @@
 import Head from 'next/head'
 import type { NextPage } from 'next';
 import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
     <div>
-    <Head>
-      <title>MSNT Telecom</title>
-    </Head>
-    
-    <header>
-      <Header />
-    </header>
+      <Head>
+        <title>MSNT Telecom</title>
+      </Head>
+
+      <header>
+        <Header />
+      </header>
+
+      {/* Hero */}
+
+      <Hero />
+
+      {/* Footer */}
+      <Footer />
 
     </div>
   )
