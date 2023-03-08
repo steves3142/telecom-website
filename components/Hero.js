d5d67050fd26b806df7688c96776c52a3aaa38bd
steves3142 <steves3142@gmail.com>
2023-03-07 23:59:05 -0500

    Revert "testing push to new branch"
    This reverts commit 9b33441d953ec14827f933c0020303b6e94d4ba7.

--- components/Hero.js
+++ components/Hero.js
@@ -28,52 +28,51 @@ function Hero() {
 
     return (
         <div>
-            <div className="">
+            <div className="relative">
 
-                <Image className="max-h-full" src={FirstPageHeader} />
+                <Image className="bg-hero-about" src={FirstPageHeader} />
 
                 <div className="absolute top-0 left-0 right-0">
                     <HeroHeader />
+                    {/* <div className="sticky top-0">
+                        {showOnScroll ?
+                            <Header /> :
+                            <div>
+                                <br></br>
+                                <br></br>
+                                <br></br>
+                            </div>}
+                    </div> */}
 
                     <main>
-                        
-                            <div className="relative py-24 sm:py-32 lg:pb-40">
-                                <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                                    <div className="mx-auto max-w-2xl text-center">
-                                        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                                            Data to enrich your online business
-                                        </h1>
-                                        <p className="mt-6 text-lg leading-8 text-gray-600">
-                                            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                                            fugiat veniam occaecat fugiat aliqua.
-                                        </p>
-                                        <div className="mt-10 flex items-center justify-center gap-x-6">
-                                            <a
-                                                href="#"
-                                                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                            >
-                                                Get started
-                                            </a>
-                                            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                                                Learn more <span aria-hidden="true">→</span>
-                                            </a>
+                        <div className="pt-10 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14">
+
+                            <div className="mx-40 max-w-6xl lg:px-8">
+                                <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+                                    <div className="">
+                                        <div className="lg:py-24">
+                                            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl lg:mt-6 xl:text-6xl">
+                                                <span className="block">A better way to</span>
+                                                <span className="block text-indigo-400">Build Telecom Infrastructure</span>
+                                            </h1>
+                                            <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                                                We're here to help you build to new heights.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="mt-12 -mb-16 sm:-mb-48 lg:relative lg:m-0">
+                                        <div className="mx-auto max-w-md px-6 sm:max-w-2xl lg:max-w-none lg:px-0">
+                                            {/* Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ */}
+                                            <img
+                                                className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                                                src=""
+                                                alt=""
+                                            />
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
-                        {/* <div className="mx-auto max-w-7xl py-16 px-6 sm:py-24 lg:px-8">
-                                        <div className="relative">
-                                            <h1 className="sr-only">
-                                                <span className="block text-xs">A better way to</span>
-                                                <span className="block text-indigo-400 text-xs">Build Telecom Infrastructure</span>
-                                            </h1>
-                                            {/* <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-sm lg:base xl:text-xl">
-                                                We're here to help you build to new heights.
-                                            </p> 
-                                        </div>
-                                        </div>
-                                        */}
+                        </div>
                         {/* More main page content here... */}
                     </main>
                 </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,27 +8,22 @@ import React from 'react';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>MSNT Telecom</title>
       </Head>
-      
-      <div className="bg-gray-100">
-        {/* Hero */}
-        <Hero />
-        <main>
-          {/* Banner */}
-          <section >
-            <Banner />
-          </section>
-        </main>
 
-        {/* Footer */}
-        <section className="relative bottom-0 left-0 right-0">
-          <Footer />
-        </section>
+      {/* Hero */}
+      <Hero />
 
-      </div>
+      {/* Banner */}
+      <main className="w-500 h-500">
+      <Banner />
+      </main>
+
+      {/* Footer */}
+      <Footer />
+
     </div>
   )
 };
