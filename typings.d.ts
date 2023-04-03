06d7eaff11cffe424f3c07e20141b66e5eb14f77
steves3142 <steves3142@gmail.com>
2023-04-02 22:33:33 -0400

    implemented api end points, created type definitions

---
+++ typings.d.ts
@@ -0,0 +1,23 @@
+interface SanityBody {
+    _createAt: string;
+    _id: string;
+    _rev: string;
+    _updatedAt: string;
+}
+
+interface Image {
+    _type: "image";
+    asset: {
+        _ref: string;
+        _type: "reference";
+    };
+}
+
+export interface Project extends SanityBody {
+    title: string;
+    _type: "project";
+    image: Image;
+    linkToBuild: string;
+    summary: string;
+    technologies: Technology[];
+}
