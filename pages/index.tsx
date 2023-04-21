80d3a97b2ecffdb11c446a4690165c57dc4dec56
steves3142 <steves3142@gmail.com>
2023-04-20 23:40:53 -0400

    testing vercel deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,9 +59,6 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
       projects,
       skills,
     },
-    // Next.js will attempt to re-generate the page:
-    //- When a request comes in 
-    //- At most once every 10 seconds
     revalidate: 10,
   };
 };
